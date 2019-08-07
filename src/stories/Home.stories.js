9388c7efdcd16277b0f000a56c28a36839799af6
Naser Masri <Naser.eng.85@hotmail.com>
2019-08-07 14:40:41 +0200

    storybook for home component

--- src/stories/Home.stories.js
+++ src/stories/Home.stories.js
@@ -4,13 +4,10 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
-import StoryRouter from 'storybook-react-router';
 import Home from '../components/Home';
 import configureStore from '../reducers/index';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
-import { Container } from '@material-ui/core';
-// import { booleanLiteral } from '@babel/types';
 
 const store = configureStore();
 
@@ -24,5 +21,5 @@ const stories = storiesOf('Home', module);
 stories
   .addDecorator(withProvider)
   .add('With Text', () => (
-    <Home spinner={false} onChange={action('onchange')} />
+    <Home spinner={false} onChange={action('onchange')} onClick={action('clicked')}/>
   ));
